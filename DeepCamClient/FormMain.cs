93d2bfb67b1206be4dbc09fb64e98da86eb4ecb1
alpha dev <alpha5611331@gmail.com>
2025-09-17 22:11:15 +0200

    changed form name

---
+++ DeepCamClient/FormMain.cs
@@ -0,0 +1,15 @@
+namespace DeepCamClient
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
