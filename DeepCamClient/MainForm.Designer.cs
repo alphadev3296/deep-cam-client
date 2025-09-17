7f5063e1885ee800c076dbf9e99b9fda25eabe93
alpha dev <alpha5611331@gmail.com>
2025-09-17 23:02:27 +0200

    done camera capture

---
+++ DeepCamClient/MainForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace DeepCamClient
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxDevices = new System.Windows.Forms.ComboBox();
+            this.buttonRefrechDevices = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.pictureBoxPreview = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxDevices
+            // 
+            this.comboBoxDevices.FormattingEnabled = true;
+            this.comboBoxDevices.Location = new System.Drawing.Point(63, 46);
+            this.comboBoxDevices.Name = "comboBoxDevices";
+            this.comboBoxDevices.Size = new System.Drawing.Size(191, 23);
+            this.comboBoxDevices.TabIndex = 0;
+            // 
+            // buttonRefrechDevices
+            // 
+            this.buttonRefrechDevices.Location = new System.Drawing.Point(260, 46);
+            this.buttonRefrechDevices.Name = "buttonRefrechDevices";
+            this.buttonRefrechDevices.Size = new System.Drawing.Size(75, 23);
+            this.buttonRefrechDevices.TabIndex = 1;
+            this.buttonRefrechDevices.Text = "Refresh";
+            this.buttonRefrechDevices.UseVisualStyleBackColor = true;
+            this.buttonRefrechDevices.Click += new System.EventHandler(this.buttonRefrechDevices_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(356, 46);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // pictureBoxPreview
+            // 
+            this.pictureBoxPreview.Location = new System.Drawing.Point(32, 100);
+            this.pictureBoxPreview.Name = "pictureBoxPreview";
+            this.pictureBoxPreview.Size = new System.Drawing.Size(694, 328);
+            this.pictureBoxPreview.TabIndex = 3;
+            this.pictureBoxPreview.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBoxPreview);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonRefrechDevices);
+            this.Controls.Add(this.comboBoxDevices);
+            this.DoubleBuffered = true;
+            this.Name = "MainForm";
+            this.Text = "Deep Cam Client";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPreview)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ComboBox comboBoxDevices;
+        private Button buttonRefrechDevices;
+        private Button buttonStart;
+        private PictureBox pictureBoxPreview;
+    }
+}
